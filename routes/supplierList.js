76f719c5e2bb8670738a2048e4b9d060093b95e2
vannknightt <nvhninojr@gmail.com>
2020-12-06 20:16:32 +0700

    update supplierList

--- routes/supplierList.js
+++ routes/supplierList.js
@@ -1,26 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql = require('mysql');
-
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "hieppro19s",
-  database: "test"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected to MySQL!");
-
-  con.query("SELECT * FROM ncc", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
-
-
 
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
